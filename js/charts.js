3a1aa9550f91c03eae7e519c57e1d6ad6d2b3875
bjohansen29 <100328437+bjohansen29@users.noreply.github.com>
2022-06-11 17:31:03 -0500

    Add files via upload

--- js/charts.js
+++ js/charts.js
@@ -77,7 +77,7 @@ function buildCharts(sample) {
       // Hint: Get the the top 10 otu_ids and map them in descending order  
       //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otu_ids.slice(0,10).map(otuID => 'OTU ${otuID}').reverse();
+    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
     var xticks = sample_values.slice(0,10).reverse();
 
     console.log(xticks);
